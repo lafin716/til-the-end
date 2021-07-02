74c4fca36a48337bed997ed7c6d34ab0509a5012
gs1902839 <pjw@gabiacns.com>
2021-07-02 14:28:28 +0900

    Initial commit

---
+++ src/main/java/com/lafin/tiltheend/TilTheEndApplication.java
@@ -0,0 +1,13 @@
+package com.lafin.tiltheend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilTheEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TilTheEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lafin/tiltheend/TilTheEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lafin.tiltheend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilTheEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
